2757c13481ec4f97a8e0d15540ffdf0946aa084d
Tyr Mactire <tyr@pettingzoo.co>
2021-11-19 15:24:35 -0800

    more test domain

--- db/memory/domain_test.go
+++ db/memory/domain_test.go
@@ -6,6 +6,123 @@ import (
 	"testing"
 )
 
+func TestClient_ReadDomain(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomain(domainExample.ID)
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	// check records
+	if d.ID != domainExample.ID {
+		t.Errorf("unexpected id, got: %s, want: %s", d.ID.String(), domainExample.ID.String())
+	}
+	if d.Domain != domainExample.Domain {
+		t.Errorf("unexpected domain, got: %s, want: %s", d.Domain, domainExample.Domain)
+	}
+}
+
+func TestClient_ReadDomain_Unknown(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomain(uuid.MustParse("75062d21-3e64-4cb5-9109-0b87e785a8dc"))
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+	if d != nil {
+		t.Errorf("unexpected domain, got: %#v, want: nil", d)
+	}
+}
+
+func TestClient_ReadDomainZ(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomainZ(domainExample.ID)
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	// check records
+	if d.ID != domainExample.ID {
+		t.Errorf("unexpected id, got: %s, want: %s", d.ID.String(), domainExample.ID.String())
+	}
+	if d.Domain != domainExample.Domain {
+		t.Errorf("unexpected domain, got: %s, want: %s", d.Domain, domainExample.Domain)
+	}
+}
+
+func TestClient_ReadDomainZ_Unknown(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomainZ(uuid.MustParse("75062d21-3e64-4cb5-9109-0b87e785a8dc"))
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+	if d != nil {
+		t.Errorf("unexpected domain, got: %#v, want: nil", d)
+	}
+}
+
+func TestClient_ReadDomainByDomain(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomainByDomain(domainTest3.Domain)
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	// check records
+	if d.ID != domainTest3.ID {
+		t.Errorf("unexpected id, got: %s, want: %s", d.ID.String(), domainTest3.ID.String())
+	}
+	if d.Domain != domainTest3.Domain {
+		t.Errorf("unexpected domain, got: %s, want: %s", d.Domain, domainTest3.Domain)
+	}
+}
+
+func TestClient_ReadDomainByDomain_Unknown(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+
+	d, err := client.ReadDomainByDomain("meowmeow.")
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil", err.Error())
+		return
+	}
+	if d != nil {
+		t.Errorf("unexpected domain, got: %#v, want: nil", d)
+	}
+}
+
 func TestClient_ReadDomainsForUser_Admin(t *testing.T) {
 	client, err := NewClient()
 	if err != nil {

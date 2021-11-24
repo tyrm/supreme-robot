39ef84f36ed478d34b2ae876c95376bfcea2f394
Tyr Mactire <tyr@pettingzoo.co>
2021-11-23 20:00:47 -0800

    test redis

--- kv/redis/domains_test.go
+++ kv/redis/domains_test.go
@@ -16,3 +16,23 @@ func TestClient_AddDomain(t *testing.T) {
 
 	tests.DoAddDomain(t, client)
 }
+
+func TestClient_GetDomains(t *testing.T) {
+	client, err := testCreateClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil.", err.Error())
+		return
+	}
+
+	tests.DoGetDomain(t, client)
+}
+
+func TestClient_RemoveDomain(t *testing.T) {
+	client, err := testCreateClient()
+	if err != nil {
+		t.Errorf("unexpected error, got: %s, want: nil.", err.Error())
+		return
+	}
+
+	tests.DoRemoveDomain(t, client)
+}
